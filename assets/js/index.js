5fb6fc749dfd380e36e9a5e2e4fdbbe4c390d93a
Julia <hijulia259@gmail.com>
2022-01-18 17:24:18 -0300

    html e css

---
+++ assets/js/index.js
@@ -0,0 +1,21 @@
+function scrollFilmes() {
+    window.scrollTo({
+        top:1000,
+        left:0,
+        behavior:"smooth"
+    })
+}
+function scrollSeries() {
+    window.scrollTo({
+        top:1400,
+        left:0,
+        behavior:"smooth"
+    })
+}
+function scrollDoc() {
+    window.scrollTo({
+        top:1800,
+        left:0,
+        behavior:"smooth"
+    })
+}
